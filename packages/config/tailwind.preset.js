205df868741142905806f39ff559e16ca5a65c2e
Demolov <demolovfennec@gmail.com>
2026-08-11 13:46:53 +0300

    add shared config package

---
+++ packages/config/tailwind.preset.js
@@ -0,0 +1,36 @@
+module.exports = {
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "16px",
+        sm: "24px",
+        lg: "40px",
+        xl: "64px",
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1440px",
+      },
+    },
+    extend: {
+      colors: {
+        "cd-ink": "#101114",
+        "cd-ink-2": "#1A1B1E",
+        "cd-ink-mute": "#6E6A65",
+        "cd-cream": "#F2EBE0",
+        "cd-paper": "#E6E2DC",
+        "cd-paper-warm": "#F5F1EC",
+        "cd-orange": "#C1552E",
+        "cd-orange-hot": "#E85A2A",
+        "cd-orange-deep": "#8F3C1E",
+        "cd-line": "#D2CCC4",
+        "cd-success": "#2E7D4F",
+        "cd-danger": "#C1341B",
+      },
+    },
+  },
+};
